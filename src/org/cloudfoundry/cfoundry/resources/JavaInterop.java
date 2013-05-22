8074611c3d4822ffac064f6c72a3d5ee19252499
Nicholas Kushmerick <nicholask@vmware.com>
2013-05-22 11:03:11 -0700

    better Java interop

--- src/org/cloudfoundry/cfoundry/resources/JavaInterop.java
+++ src/org/cloudfoundry/cfoundry/resources/JavaInterop.java
@@ -1,27 +1,13 @@
 package org.cloudfoundry.cfoundry.resources;
 
-import java.lang.Iterable;
-import java.util.*;
-
-/*
- * Ugg, is there a better way -- scala.collection.JavaConversions loses type parameters?!
- */
-
 public class JavaInterop {
 
 	public static Resource asResource(Magic magic) {
 		return magic.resource();
 	}
 
-	public static Iterable<Resource> asResources(Magic magic) {
-		scala.collection.Seq<Resource> resources = magic.resources();
-		Resource[] result = new Resource[resources.size()];
-		scala.collection.Iterator<Resource> i = resources.iterator();
-		int j = 0;
-		while (i.hasNext()) {
-			result[j++] = i.next(); // TODO: Avoid copying
-		}
-		return Arrays.asList(result);
+	public static java.lang.Iterable<Resource> asResources(Magic magic) {
+		return scala.collection.JavaConversions.asJavaIterable(magic.resources());
 	}
 
 }
